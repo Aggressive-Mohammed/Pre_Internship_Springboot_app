b0ebf2624c6411d0fb7ffc207dcc9559c2038008
ibrahimmohammedvis@gmail.com <ibrahimmohammedvis@gmail.com>
2023-07-26 17:13:18 -0700

    intial commit

---
+++ src/main/java/com/aggressive/preinternshipapp/PreinternshipappApplication.java
@@ -0,0 +1,13 @@
+package com.aggressive.preinternshipapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreinternshipappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreinternshipappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aggressive/preinternshipapp/PreinternshipappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggressive.preinternshipapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreinternshipappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
